59179a3855986dab7755a34c465d3704f6fda1ea
Vladislav Netaev <reckchant@gmail.com>
2023-12-06 12:21:35 +0700

    Day 6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,8 +1,17 @@
 fun main() {
-    val count = readln().toInt()
-    val lines = (1..count).map { readln() }
+    val count = 2
+    val lines = (1..count).map { readln().drop(9) }
 
-    var answer = 0
+    val time = lines.first().split(' ').filterNot { it.isBlank() }.map { it.toInt() }.joinToString("").toLong()
+    val distances = lines.last().split(' ').filterNot { it.isBlank() }.map { it.toInt() }.joinToString("").toLong()
 
-    println(answer)
+    var timeAnswer = 0
+    for (i in 0..time) {
+        val left = time - i
+        val distance = i * left
+        if (distance > distances) {
+            timeAnswer += 1
+        }
+    }
+    println(timeAnswer)
 }
